f8a13aa35126b65a96cf148d63a6b090ff14b57f
Mr_CZ <craigz@eagles.ewu.edu>
2019-02-11 19:27:28 -0800

    Interface for Zip

---
+++ MonAmie/MonAmieData/Interfaces/IZipService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MonAmieData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MonAmieData.Interfaces
+{
+    public interface IZipService
+    {
+        void ZipService();
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        void ZipService(ZipContext context);
+
+
+        /// <summary>
+        /// Get all zipcodes stored in the database
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<ZIP_Codes> GetAllUsers();
+
+
+        /// <summary>
+        /// gets the city infromation by zipcode
+        /// </summary>
+        /// <param name="zipCode"></param>
+        /// <returns></returns>
+        ZIP_Codes GetByZip(int zipCode);
+
+
+        /// <summary>
+        /// gets the state name by state code
+        /// </summary>
+        /// <param name="stateCode"></param>
+        /// <returns></returns>
+        States GetByStateCode(int stateCode);
+
+
+        /// <summary>
+        /// returns the name of the city and state in a string
+        /// </summary>
+        /// <param name="zipCode"></param>
+        /// <returns></returns>
+        string getCityandState(int zipCode);
+
+    }
+}
--- MonAmie/MonAmieServices/ZipService.cs
+++ MonAmie/MonAmieServices/ZipService.cs
@@ -7,7 +7,7 @@ using System.Linq;
 
 namespace MonAmieServices
 {
-    public class ZipService
+    public class ZipService : IZipService
     {
         private ZipContext _context;
 
