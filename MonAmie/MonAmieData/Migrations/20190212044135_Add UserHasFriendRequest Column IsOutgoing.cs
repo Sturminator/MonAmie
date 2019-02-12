5b045bab96a7aa4a4fdbf904d6cefa1a873bfb34
Christopher Sturm <csturm1994@gmail.com>
2019-02-11 20:42:29 -0800

    added column to UserHasFriendRequest table to track whether it is an incoming or outgoing friend request.

---
+++ MonAmie/MonAmieData/Migrations/20190212044135_Add UserHasFriendRequest Column IsOutgoing.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MonAmieData.Migrations
+{
+    public partial class AddUserHasFriendRequestColumnIsOutgoing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOutgoing",
+                table: "UserHasFriendRequest",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOutgoing",
+                table: "UserHasFriendRequest");
+        }
+    }
+}
--- MonAmie/MonAmieData/Models/UserHasFriendRequest.cs
+++ MonAmie/MonAmieData/Models/UserHasFriendRequest.cs
@@ -13,6 +13,9 @@ namespace MonAmieData.Models
         [Required]
         public int PendingFriendId { get; set; }
 
+        [Required]
+        public bool IsOutgoing { get; set; }
+
         public virtual User User { get; set; }
         public virtual User PendingFriend { get; set; }
     }
