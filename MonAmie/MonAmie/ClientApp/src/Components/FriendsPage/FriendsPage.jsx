c4c227efddfb021ea50f981db1741a2e353e4daf
Christopher Sturm <csturm1994@gmail.com>
2019-02-17 13:40:00 -0800

    added template for friends page

---
+++ MonAmie/MonAmie/ClientApp/src/Components/FriendsPage/FriendsPage.jsx
@@ -0,0 +1,26 @@
+﻿import React, { Component } from 'react';
+
+class FriendsPage extends Component {
+    state = {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+    }
+}
+
+function mapStateToProps(state) {
+    const { authentication } = state;
+    const { user } = authentication;
+    return {
+        user
+    };
+}
+
+const connectedFriendsPage = connect(mapStateToProps)(FriendsPage);
+export { connectedFriendsPage as FriendsPage };
